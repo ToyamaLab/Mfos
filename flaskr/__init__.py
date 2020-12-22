b7cc4fc85528fde15949836c7b29fbefbcc2b2ae
“kazu” <kazu@db.ics.keio.ac.jp>
2020-12-22 17:19:25 +0900

    fix

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,7 +6,7 @@ import flaskr.main.models
 def create_app():
     app = Flask(__name__)
     # configを読み込む
-    # app.config.from_object('flaskr.config.Config')
+    app.config.from_object('flaskr.config.Config')
 
     # DBを初期化
     init_db(app)
--- flaskr/config.py
+++ flaskr/config.py
@@ -1,25 +1,25 @@
-# import os
-#
-#
-# class DevelopmentConfig:
+import os
+
+
+class DevelopmentConfig:
 #
 #     # Flask
 #     DEBUG = False
 #
-#     # SQLAlchemy
-#     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/mfos-db?charset=utf8'.format(**{
-#         'user': os.getenv('DB_USER', 'root'),
-#         'password': os.getenv('DB_PASSWORD', 'password'),
-#         'host': os.getenv('DB_HOST', 'localhost'),
-#     })
-#     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db}?charset=utf8'.format(**{
-#     #     'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
-#     #     'password': os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
-#     #     'host': os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
-#     #     'db': os.getenv('DB_NAME', os.environ['DB_NAME']),
-#     # })
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     SQLALCHEMY_ECHO = False
-#
-#
-# Config = DevelopmentConfig
+    # SQLAlchemy
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/mfos-db?charset=utf8'.format(**{
+    #     'user': os.getenv('DB_USER', 'root'),
+    #     'password': os.getenv('DB_PASSWORD', 'password'),
+    #     'host': os.getenv('DB_HOST', 'localhost'),
+    # })
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db}?charset=utf8'.format(**{
+        'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
+        'password': os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
+        'host': os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
+        'db': os.getenv('DB_NAME', os.environ['DB_NAME']),
+    })
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+
+
+Config = DevelopmentConfig
