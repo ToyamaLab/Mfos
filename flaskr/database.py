6045e6a07776667d4e6c77ce02fe3c8d5d1e1aac
“kazu” <kazu@db.ics.keio.ac.jp>
2020-12-22 16:50:54 +0900

    update database

--- flaskr/database.py
+++ flaskr/database.py
@@ -13,19 +13,29 @@ def init_db(app):
 
 
 def connect_db():
-    try:
-        db_conn = mydb.connect(
-            host=os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
-            user=os.getenv('DB_USER', os.environ['DB_USERNAME']),
-            password=os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
-            database=os.getenv('DB_NAME', os.environ['DB_NAME']),
-            port=3306
-        )
-    except KeyError:
+    if os.environ['DB_HOSTNAME'] is None:
         db_conn = mydb.connect(
             user='root',
             password='password',
             database='mfos-db',
             port=3306
         )
+        
+    else:
+        db_conn = mydb.connect(
+            host=os.getenv(os.environ['DB_HOSTNAME']),
+            user=os.getenv(os.environ['DB_USERNAME']),
+            password=os.getenv(os.environ['DB_PASSWORD']),
+            database=os.getenv(os.environ['DB_NAME']),
+            port=3306
+        )
+        # db_conn = mydb.connect(
+        #     host=os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
+        #     user=os.getenv('DB_USER', os.environ['DB_USERNAME']),
+        #     password=os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
+        #     database=os.getenv('DB_NAME', os.environ['DB_NAME']),
+        #     port=3306
+        # )
+    # except KeyError:
+
     return db_conn
